dec9bc88466389cc12f867e302cc47e8b3ad3d3b
vinassya-gif <vsupiyanti@gmail.com>
2026-09-21 10:48:21 +0700

    menambahkan div alet di berita-detail

--- resources/views/berita-detail.blade.php
+++ resources/views/berita-detail.blade.php
@@ -15,5 +15,10 @@
     <div class="content-card">
         <p style="white-space:pre-line;">{{ $berita->isi }}</p>
     </div>
+
+    <div class="alert">
+        span class="text-muted">Bagikan berita ini:</span>
+    </div>
+    
 </div>
 @endsection
